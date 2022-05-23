b063acd33f2aabb4eb61bb5641459d787d306662
anoushk1234 <anoushka1828@gmail.com>
2022-05-23 21:29:05 +0530

    major feature: support for discord embeds

---
+++ src/types/index.ts
@@ -0,0 +1,32 @@
+export interface MessageEmbed {
+  author: Author;
+  title: string;
+  url: string;
+  description: string;
+  color: number;
+  fields: Field[];
+  thumbnail: Image;
+  image: Image;
+  footer: Footer;
+}
+
+export interface Author {
+  name: string;
+  url: string;
+  iconURL: string;
+}
+
+export interface Field {
+  name: string;
+  value: string;
+  inline?: boolean;
+}
+
+export interface Footer {
+  text: string;
+  iconURL: string;
+}
+
+export interface Image {
+  url: string;
+}
